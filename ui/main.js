4706ee82faadd54bfb0b37321b09493921a34e32
Neha-Panwar <Neha-Panwar@users.noreply.github.com>
2016-10-12 20:05:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,13 @@
 console.log('Loaded!');
+
 //change in the main text
+
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+//move the image
+
+var img = document.getElementById('img');
+img.onclick=function(){
+  img.style.marginLeft='100px';  
+};
