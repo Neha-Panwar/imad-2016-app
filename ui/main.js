ed720fea7d0d427661290c15f9d3e62e90a6f4c9
Neha-Panwar <Neha-Panwar@users.noreply.github.com>
2016-11-11 20:09:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,42 +1,123 @@
-//counter code
-var button = document.getElementById('counter');
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-button.onclick = function (){
-   // Create a request object
-   var request = new XMLHttpRequest();
-   
-   //Capture the response and store it in a variable
-   request.onreadystatechange = function (){
-     if(request.readyState === XMLHttpRequest.DONE){
-         // Take some action
-         if(request.status === 200){
-             var counter = request.responseText;
-             var span = document.getElementById('count');
-             span.innerHTML = counter.toString();
-             
-         }
-     }  
-   };
-   // Make a request
-   request.open('GET', 'http://neha-panwar.imad.hasura-app.io/counter', true);
-   request.send(null);
-};
- // submit name
- 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-  // Make a request to the server and send the name
+var app = express();
+app.use(morgan('combined'));
+
+var articles= {
+'article-one': {
+  title: 'Article One | Neha Panwar',
+  heading: 'Article One',
+  date: 'Sep 5,2016',
+  content: `<p>
+                This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+            </p>
+            
+            <p>
+                This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+            </p>
+            <p>
+                This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
+            </p>`
   
-  // Capture a list of names and render it as a list
-  var names = ['name1', 'name2' , 'name3', 'name4'];
-  var list = '';
-  for(var i=0;i<names.length;i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+},
+'article-two': {
+    title: 'Article Two | Neha Panwar',
+  heading: 'Article Two',
+  date: 'Sep 6,2016',
+  content: `<p>
+                This is the content of my second article.
+            </p>`
+            
+         },
+'article-three': {
+    title: 'Article Three | Neha Panwar',
+  heading: 'Article Three',
+  date: 'Sep 7,2016',
+  content: `<p>
+                This is the content of my third article.
+            </p>`
+            
+           
+}
 };
- 
+function createTemplate(data){
+var title= data.title;
+var date= data.date;
+var heading= data.heading;
+var content= data.content;
+var htmlTemplate=`
+<head>
+    <title>
+     ${title}
+        </title>
+        <meta name="viewport" content="width=device-width , initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+         <div>
+         <a href="/">home</a>
+         </div>
+         <hr/>
+         <h3>
+            ${heading}
+         </h3>
+        
+         <div>
+             ${date}
+         </div>
+        
+         <div>
+           ${content}
+         </div>
+    </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter=0;
+app.get('/counter',function(req,res){
+counter=counter + 1;
+res.send(counter.toString());
+
+});
+
+var names=[];
+app.get('/submit-name', function (req, res){ //URL:/submit-name?name=XXXXX
+    //Get the name from request object
+    var name = req.query.name;
+    names.push(name);
+    //JSON javascript object notation
+    res.send(JSON.stringify(names));
+});
+
+app.get('/:articleName',function (req, res){
+    var articleName = req.params.articleName;
+ res.send(createTemplate(articles[articleName]));  
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
