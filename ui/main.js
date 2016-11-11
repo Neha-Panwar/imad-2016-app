31cca818c7d13c02eff668ce38d52215838c8977
Neha-Panwar <Neha-Panwar@users.noreply.github.com>
2016-11-11 21:22:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,14 +29,30 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    // Make a request to the server and send the name
     
-    //Capture a list of names and render as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for(var i = 0; i < names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.Done) {
+            //Take some action
+            if (request.status === 200) {
+            //Capture a list of names and render as a list
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i = 0; i < names.length; i++){
+            list += '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+            }
+        }
+        // Not Done Yet
+    };
+       
+    //Make a request
+    request.open('GET', 'http://neha-panwar.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
 };
